96f9a525b1eb0c88cf0caf4f37904fae07f5d39c
Madhavsaikumar <84717045+Madhavsaikumar@users.noreply.github.com>
2021-06-04 11:03:09 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -4,7 +4,24 @@
 #include <math.h>
 
 float wallis_pi(int);
-
+float wallis_pi (int n )
+{
+  float pi=1;
+  float a,b,comp_pi;
+   
+   for (int i=1;i<n+1;i++)
+  {
+    a= 4*i*i;
+    b=a/(a-1);
+  
+    pi = pi*b;
+   
+  }
+  if(n!=0)
+  return (2*pi);
+  else
+  return 0;
+    }
 int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
